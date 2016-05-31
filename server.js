89e2fc783d69adc902086c2100c81ec3ba7abb73
Jorge <ygd182@gmail.com>
2016-05-30 21:14:01 -0300

    heroku deploy2

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ var app = require('./app');
 //db.connect(config);
 db.connect(app.get('dbUrl'));
 
-
-// Start Express
-var server = app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + server.address().port);
+var dbConnection = db.connection; 
+dbConnection.on( 'error' , console.error.bind(console, 'connection error:' )); 
+dbConnection.once( 'open' , function () { 
+	// we're connected! 
+	startServer();
 });
+
+function startServer() {
+	// Start Express
+	var server = app.listen(app.get('port'), function() {
+	    console.log('Express server listening on port ' + server.address().port);
+	});
+
+}
